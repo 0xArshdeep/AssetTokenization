1e70064e90980987acdbc8ca1e3e2a370b5c20e0
arshsb77 <arshsb1998@gmail.com>
2022-04-08 15:51:24 -0700

    Adding more unit tests to MyToken.

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -12,6 +12,8 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 contract("Token Test", async (accounts) => {
+
+    const [deployerAccount, recipient, anotherAccount]  = accounts;
     
     it("All tokens should be in my account", async () => {
         let instance = await Token.deployed();
@@ -23,7 +25,26 @@ contract("Token Test", async (accounts) => {
         //expect(await instance.balanceOf(accounts[0])).to.be.a.bignumber.equal(totalSupply);
 
         // Cleaner way using chai-as-promised 
-        expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+    });
+
+    it("is not possible to send more tokens than available in total", async() => {
+        let instance = await Token.deployed();
+        let balanceOfDeployer = await instance.balanceOf(deployerAccount);
+
+        expect(instance.transfer(recipient,new BN(balanceOfDeployer+1))).to.eventually.be.rejected;
+
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
+    });
+
+    it("is possible to send tokens between accounts", async() => {
+        const sendTokens = 1;
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equals(totalSupply.sub(new BN(sendTokens)));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equals(new BN(sendTokens));
     });
 
 });
