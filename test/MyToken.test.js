666b1d0daf43225b35f597a108ab364bb3ace1b1
arshsb77 <arshsb1998@gmail.com>
2022-04-08 17:22:22 -0700

    Updates unit tests to account for Crowdsale

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -14,9 +14,13 @@ const expect = chai.expect;
 contract("Token Test", async (accounts) => {
 
     const [deployerAccount, recipient, anotherAccount]  = accounts;
+
+    beforeEach(async() => {
+        this.myToken = await Token.new(1000000)
+    });
     
     it("All tokens should be in my account", async () => {
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let totalSupply = await instance.totalSupply();
 
         //let balance = await instance.balanceOf(accounts[0]);
@@ -29,7 +33,7 @@ contract("Token Test", async (accounts) => {
     });
 
     it("is not possible to send more tokens than available in total", async() => {
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let balanceOfDeployer = await instance.balanceOf(deployerAccount);
 
         expect(instance.transfer(recipient,new BN(balanceOfDeployer+1))).to.eventually.be.rejected;
@@ -39,7 +43,7 @@ contract("Token Test", async (accounts) => {
 
     it("is possible to send tokens between accounts", async() => {
         const sendTokens = 1;
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let totalSupply = await instance.totalSupply();
         expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
         expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
